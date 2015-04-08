9edc0e93a4f102782a551821adfda5cbde173114
Paramesh Vijay <paramesh.padmanabhan@listertechnologies.com>
2015-04-08 18:25:29 +0530

    First Commit

---
+++ src/redMart/Catalog.java
@@ -0,0 +1,56 @@
+package redMart;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Catalog extends Home {
+	public static int cart_quantity = 0;
+	@SuppressWarnings("deprecation")
+	@Test (dependsOnMethods = { "Log_In" })
+	public void Catalog_Without_Search() throws IOException, InterruptedException {
+		
+		//waiting until the text "My Cart" is displayed post logged in
+		Thread.sleep(10000);
+		wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(Object.getProperty("CatalogPage.Signed_User_Home_Page"))));
+		
+		//Asserting the quantity being displayed in the "Cart" link in the header section
+		assert wait.until(ExpectedConditions.textToBePresentInElement(By.xpath(Object.getProperty("CatalogPage.Signed_User_Home_Page")), "My Cart"));
+		String zero_quantity = "My Cart";
+		assert driver.findElement(By.xpath(Object.getProperty("CatalogPage.Signed_User_Home_Page"))).getText().equals(zero_quantity);
+		driver.findElement(By.id(Object.getProperty("HomePage.Logo"))).click();
+		
+		Add_Item();
+		//Mouse Hover "Cart" link function
+		Cart_Quantity();
+	}
+		
+		
+	//This function will be called whenever there is a need to verify the number of items or quantity of the cart
+	private void Cart_Quantity() {
+		//Performing Mouse Hover action
+		Actions action = new Actions(driver);
+		WebElement Cart = driver.findElement(By.xpath(Object.getProperty("CatalogPage.Cart_Post_Item_Addition")));
+		action.moveToElement(Cart).build().perform();
+		//List test = (List) driver.findElements(By.xpath(Object.getProperty("CatalogPage.Quantity"))).get(2);
+		assert Integer.parseInt(driver.findElement(By.xpath(Object.getProperty("CatalogPage.Quantity"))).getText()) == cart_quantity;
+		//char quantity = driver.findElement(By.xpath(Object.getProperty("CatalogPage.Quantity"))).getText().toString().charAt(2);
+	}
+
+	//This function will be called whenever there is a need to Add items to the cart
+	public void Add_Item() throws InterruptedException {
+		driver.findElement(By.xpath(Object.getProperty("CatalogPage.Item_Description"))).click();
+		driver.findElement(By.xpath(Object.getProperty("CatalogPage.Add_To_Cart_Button"))).click();
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(Object.getProperty("CatalogPage.Cart_Quantity"))));
+		cart_quantity++;
+		System.out.println(cart_quantity);
+		Cart_Quantity();
+		Thread.sleep(5000);
+	}
+}
---
+++ src/redMart/Home.java
@@ -0,0 +1,72 @@
+package redMart;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Home {
+	public static WebDriver driver;
+	public static WebDriverWait wait;	
+	public static Properties Object = new Properties();
+	
+	@BeforeTest()
+	public void Load_Home_Page_Objects() throws IOException{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Paramesh_p\\Desktop\\Paramesh\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://redmart.com");
+		driver.manage().window().maximize();
+	}
+	
+	@BeforeMethod()
+	public void Object_Read() throws IOException {
+		FileInputStream HomeObjRep = new FileInputStream(System.getProperty("user.dir")+"\\src\\redMartObject\\Home_Page_Web_Elements.OR");
+		//Object = new Properties();
+		Object.load(HomeObjRep);
+	}
+	
+	@Test
+	public void Log_In() throws IOException, InterruptedException{
+
+			wait = new WebDriverWait(driver, 10);
+			try {
+				//waiting until the "Huge Product Range" div is getting loaded
+				
+				Thread.sleep(2000);
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Object.getProperty("HomePage.Huge_Product_Range_Image"))));
+				}
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+			//Trying to login by clicking the link "Log in"
+			driver.findElement(By.id(Object.getProperty("HomePage.LogIn"))).click();
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginBarInner")));
+			
+			//clearing off the contents as it is creating problems when keying in the email. Often only partial text is being entered
+			driver.findElement(By.xpath(Object.getProperty("HomePage.SignIn_Email"))).clear();
+			Thread.sleep(5000);
+			
+			driver.findElement(By.xpath(Object.getProperty("HomePage.SignIn_Email"))).sendKeys("parameshprof@gmail.com");
+			driver.findElement(By.xpath(Object.getProperty("HomePage.SignIn_Password"))).sendKeys("test123");
+			driver.findElement(By.xpath(Object.getProperty("HomePage.SignIn_Button"))).click();
+		}
+	
+	/*@AfterTest
+	public void Close_Browser() {
+		//driver.close();
+		driver.quit();
+	}*/
+}
